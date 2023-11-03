d492f63a6cc306ea00e7ad38ee259171e15aeb5e
aparru33 <adrien.parruitte@gmail.com>
2023-11-03 12:06:54 +0100

    model

--- Fruit_classification/model/custom_model.py
+++ Fruit_classification/model/custom_model.py
@@ -1,6 +1,100 @@
 import tensorflow as tf
-from tensorflow.keras import layers
+from tensorflow.keras import layers, models
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+from tensorflow.keras.metrics import Precision, Recall, AUC
+from sklearn.metrics import classification_report
 import numpy as np
-import os
+
+def create_model(input_shape, num_classes):
+    model = models.Sequential()
+
+    # Input Layer
+    model.add(layers.Input(shape=input_shape))
+
+    # Normalization Layer
+    model.add(layers.Rescaling(1./255))
+
+    # First Convolutional Block
+    model.add(layers.Conv2D(32, (3, 3), activation='relu'))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+    # Max Pooling Layer
+    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+
+    # Dropout Layer
+    model.add(layers.Dropout(0.25))
+
+    # Flatten Layer
+    model.add(layers.Flatten())
+
+    # Dense Layer
+    model.add(layers.Dense(num_classes, activation='softmax'))
+
+    # Compile the Model
+    model.compile(optimizer='adam', 
+                  loss='categorical_crossentropy', 
+                  metrics=['accuracy', Precision(), Recall(), AUC()])
+
+    return model
+
+# Set the path to your dataset
+TRAIN_DIR = 'D:/Workspace_Python/DeepLearning/Fruit_classification/dataset/train'
+VALIDATION_DIR = 'D:/Workspace_Python/DeepLearning/Fruit_classification/dataset/validation'
+
+# Bilinear, bicubic, lanczos5, mitchellcubic.
+INTERPOLATION = "bilinear"
+
+# Load dataset
+batch_size = 32
+img_height = 64
+img_width = 64
+train_dataset = image_dataset_from_directory(
+    TRAIN_DIR,
+    validation_split=0.2,
+    subset="training",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size,
+    label_mode='categorical'
+)
+validation_dataset = image_dataset_from_directory(
+    VALIDATION_DIR,
+    validation_split=0.2,
+    subset="validation",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size,
+    label_mode='categorical'
+)
+
+# Get number of classes
+num_classes = len(train_dataset.class_names)
+print(num_classes)
+# Create and compile the model
+model = create_model((img_height, img_width, 3), num_classes)
+
+# Train the model
+epochs = 10
+model.fit(
+  train_dataset,
+  epochs=epochs
+)
 
 
+# Evaluate the model on the test dataset
+results = model.evaluate(validation_dataset)
+print("Test Loss:", results[0])
+print("Test Accuracy:", results[1])
+print("Test Precision:", results[2])
+print("Test Recall:", results[3])
+print("Test AUC:", results[4])
+
+# For detailed classification report (optional)
+y_true = []
+y_pred = []
+for img_batch, label_batch in validation_dataset:
+    preds = model.predict(img_batch)
+    y_true.extend(np.argmax(label_batch.numpy(), axis=1))
+    y_pred.extend(np.argmax(preds, axis=1))
+
+print(classification_report(y_true, y_pred, target_names=train_dataset.class_names))
--- Fruit_classification/model/data_augmentation.py
+++ Fruit_classification/model/data_augmentation.py
@@ -1,7 +1,8 @@
+#%%
 import numpy as np
 import os
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
-
+#%%
 # Define the directory containing your signature dataset
 #dataset_dir = 'train/set'
 
@@ -55,3 +56,74 @@ for d in directories:
             i += 1
             if i >= 10:  # Generate 10 augmented images for each original image
                 break
+
+#%%
+import os
+import pandas as pd
+import shutil
+
+# Paths
+csv_file_path = 'D:/Workspace_Python/DeepLearning/Fruit_classification/dataset/sampleSubmission.csv'
+images_directory_path = 'D:/Workspace_Python/DeepLearning/Fruit_classification/dataset/test'
+output_directory_path = 'D:/Workspace_Python/DeepLearning/Fruit_classification/dataset/sorted_test'
+
+# Read CSV file
+df = pd.read_csv(csv_file_path)
+
+# Ensure 'id' is in string format and properly formatted
+df['id'] = df['id'].apply(lambda x: str(x).zfill(4))
+
+# Create directories for each label
+for label in df['label'].unique():
+    label_dir = os.path.join(output_directory_path, str(label))
+    if not os.path.exists(label_dir):
+        print("create", label_dir)
+        os.makedirs(label_dir)
+
+#%%
+import os
+import shutil
+from math import ceil
+import random
+
+def move_files(src_dir, dst_dir, percentage=0.2):
+    if not os.path.exists(src_dir):
+        print("Source directory does not exist!")
+        return
+    if not os.path.exists(dst_dir):
+        os.makedirs(dst_dir)
+
+    # Loop through each subfolder in the source directory
+    for subfolder in os.listdir(src_dir):
+        subfolder_path = os.path.join(src_dir, subfolder)
+
+        # Check if it's a directory
+        if os.path.isdir(subfolder_path):
+            files = os.listdir(subfolder_path)
+            files = [f for f in files if os.path.isfile(os.path.join(subfolder_path, f))]  # Filter out subdirectories
+            num_files_to_move = ceil(percentage * len(files))
+
+            # Randomly select files to move
+            files_to_move = random.sample(files, num_files_to_move)
+
+            # Create corresponding subfolder in destination directory if it doesn't exist
+            dst_subfolder_path = os.path.join(dst_dir, subfolder)
+            if not os.path.exists(dst_subfolder_path):
+                os.makedirs(dst_subfolder_path)
+
+            # Move files
+            for file in files_to_move:
+                src_file_path = os.path.join(subfolder_path, file)
+                dst_file_path = os.path.join(dst_subfolder_path, file)
+                shutil.move(src_file_path, dst_file_path)
+
+            print(f"Moved {num_files_to_move} files from {subfolder_path} to {dst_subfolder_path}")
+
+print("Moving files...")
+move_files(
+    'D:/Workspace_Python/DeepLearning/Fruit_classification/dataset/train', 
+    'D:/Workspace_Python/DeepLearning/Fruit_classification/dataset/validation')
+print("Files moved successfully!")
+
+
+# %%
