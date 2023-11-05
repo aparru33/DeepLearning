2bda2a7392a385ddd2550976daebed93c144b414
aparru33 <adrien.parruitte@gmail.com>
2023-11-05 21:33:45 +0000

    first model

--- Fruit_classification/model/custom_model.py
+++ Fruit_classification/model/custom_model.py
@@ -38,8 +38,8 @@ def create_model(input_shape, num_classes):
     return model
 
 # Set the path to your dataset
-TRAIN_DIR = 'D:/Workspace_Python/DeepLearning/Fruit_classification/dataset/train'
-VALIDATION_DIR = 'D:/Workspace_Python/DeepLearning/Fruit_classification/dataset/validation'
+TRAIN_DIR = '/home/ubuntu/workspace/finovox_main/dl_project/DeepLearning/Fruit_classification/dataset/train'
+VALIDATION_DIR = '/home/ubuntu/workspace/finovox_main/dl_project/DeepLearning/Fruit_classification/dataset/validation'
 
 # Bilinear, bicubic, lanczos5, mitchellcubic.
 INTERPOLATION = "bilinear"
@@ -48,53 +48,71 @@ INTERPOLATION = "bilinear"
 batch_size = 32
 img_height = 64
 img_width = 64
-train_dataset = image_dataset_from_directory(
-    TRAIN_DIR,
-    validation_split=0.2,
-    subset="training",
-    seed=123,
-    image_size=(img_height, img_width),
-    batch_size=batch_size,
-    label_mode='categorical'
-)
-validation_dataset = image_dataset_from_directory(
-    VALIDATION_DIR,
-    validation_split=0.2,
-    subset="validation",
-    seed=123,
-    image_size=(img_height, img_width),
-    batch_size=batch_size,
-    label_mode='categorical'
-)
 
-# Get number of classes
-num_classes = len(train_dataset.class_names)
-print(num_classes)
-# Create and compile the model
-model = create_model((img_height, img_width, 3), num_classes)
+try:
+    log = open("/home/ubuntu/workspace/finovox_main/dl_project/DeepLearning/Fruit_classification/model/log.txt", "w")
+    log.write("Load datasets\n")
 
-# Train the model
-epochs = 10
-model.fit(
-  train_dataset,
-  epochs=epochs
-)
+    train_dataset = image_dataset_from_directory(
+        TRAIN_DIR,
+        validation_split=0.2,
+        subset="training",
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size,
+        label_mode='categorical'
+    )
+    log.write("train datasets loaded\n")
+    validation_dataset = image_dataset_from_directory(
+        VALIDATION_DIR,
+        validation_split=0.2,
+        subset="validation",
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size,
+        label_mode='categorical'
+    )
+    log.write("validation datasets loaded\n")
+    # Get number of classes
+    log.write("Get number of classes\n")
+    num_classes = len(train_dataset.class_names)
+    # Create and compile the model
+    log.write("Create and compile the model\n")
+    model = create_model((img_height, img_width, 3), num_classes)
+    log.write("Model created\n")
+    # Train the model
+    epochs = 10
+    log.write(f"Train the model with {epochs}\n")
+    epochs = 10
+    model.fit(
+    train_dataset,
+    epochs=epochs
+    )
+    log.write("Model trained\n")
 
+    log.write("Save the model\n")
+    model.save('/home/ubuntu/workspace/finovox_main/dl_project/DeepLearning/Fruit_classification/model/model.h5')
+    log.write("Model saved\n")
+    # Evaluate the model on the test dataset
+    results = model.evaluate(validation_dataset)
 
-# Evaluate the model on the test dataset
-results = model.evaluate(validation_dataset)
-print("Test Loss:", results[0])
-print("Test Accuracy:", results[1])
-print("Test Precision:", results[2])
-print("Test Recall:", results[3])
-print("Test AUC:", results[4])
+    log.write(f"Test Loss = {results[0]}")
+    log.write(f"Test Accuracy = {results[1]}")
+    log.write(f"Test Precision = {results[2]}")
+    log.write(f"Test Recall = {results[3]}")
+    log.write(f"Test AUC = {results[4]}")
 
-# For detailed classification report (optional)
-y_true = []
-y_pred = []
-for img_batch, label_batch in validation_dataset:
-    preds = model.predict(img_batch)
-    y_true.extend(np.argmax(label_batch.numpy(), axis=1))
-    y_pred.extend(np.argmax(preds, axis=1))
+    # For detailed classification report (optional)
+    y_true = []
+    y_pred = []
+    for img_batch, label_batch in validation_dataset:
+        preds = model.predict(img_batch)
+        y_true.extend(np.argmax(label_batch.numpy(), axis=1))
+        y_pred.extend(np.argmax(preds, axis=1))
 
-print(classification_report(y_true, y_pred, target_names=train_dataset.class_names))
+    print(classification_report(y_true, y_pred, target_names=train_dataset.class_names))
+except Exception as e:
+    log.write("Error occured\n")
+    log.write(str(e))
+finally:
+    log.close()
