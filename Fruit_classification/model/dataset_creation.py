40f9c5fe301391ce34eb8ca09919d82f21475ce5
aparru33 <adrien.parruitte@gmail.com>
2023-11-24 18:57:16 +0000

    finish dataset creation

--- Fruit_classification/model/dataset_creation.py
+++ Fruit_classification/model/dataset_creation.py
@@ -134,11 +134,13 @@ ZHANG_DATASET = PROJECT_PATH + "dataset/fruit_classification_zhang/"
 FRUIT_360_DATASET = PROJECT_PATH + "dataset/Fruits_360/"
 FRUITS_VEGETABLES_DATASET = PROJECT_PATH + "dataset/Fruits_and_Vegetables_Image_Recognition_Dataset/"
 IMAGES_DATASET = PROJECT_PATH + "dataset/images/"
+FINAL_DATASET = '/home/ubuntu/workspace/finovox_main/dl_project/DeepLearning/Fruit_classification/dataset_original_size_before_augment/'
+FINAL_DATASET_224 = '/home/ubuntu/workspace/finovox_main/dl_project/DeepLearning/Fruit_classification/dataset/final_dataset/'
 #%%
-# for d in os.listdir(IMAGES_DATASET):
-#     if not os.path.exists(PROJECT_PATH+'dataset/train/'+d):
-#         os.mkdir(PROJECT_PATH+'dataset/train/'+d)
-#     move_files(IMAGES_DATASET+d, PROJECT_PATH+'dataset/train/'+d, 0.8)
+for d in os.listdir(FINAL_DATASET+'train'):
+    if not os.path.exists(FINAL_DATASET+'test/'+d):
+        os.mkdir(FINAL_DATASET+'test/'+d)
+    move_files(FINAL_DATASET+'train/'+d, FINAL_DATASET+'test/'+d, 0.2)
 
 # #%%
 # for d in os.listdir(IMAGES_DATASET):
@@ -146,13 +148,21 @@ IMAGES_DATASET = PROJECT_PATH + "dataset/images/"
 #         os.mkdir(PROJECT_PATH+'dataset/test/'+d)
 #     move_files(IMAGES_DATASET+d, PROJECT_PATH+'dataset/test/'+d, 1)
 
-# #%%
-# for item in os.listdir(IMAGES_DATASET):
-#     item_path = os.path.join(IMAGES_DATASET, item)
-#     if os.path.isdir(item_path):
-#         new_name = item.split()[0]
-#         new_path = os.path.join(IMAGES_DATASET, new_name)
-#         os.rename(item_path, new_path)
+#%%
+for item in os.listdir(IMAGES_DATASET+'/images'):
+    item_path = os.path.join(IMAGES_DATASET+'/images', item)
+    if os.path.isdir(item_path):
+        new_name = item.split()[0]
+        new_name = new_name[0].lower()+new_name[1:]
+        new_path = os.path.join(IMAGES_DATASET+'/images', new_name)
+        # print(new_path)
+        # if new_name in os.listdir(FINAL_DATASET+'/train/'):
+        #     for f in os.listdir(item_path):
+        #         shutil.move(FINAL_DATASET+'/train/'+item+'/' + f, 
+        #                     FINAL_DATASET+'/train/'+new_name+'/' + f)
+        #     shutil.rmtree(item_path)
+        # else:
+        os.rename(item_path, new_path)
 
 # #%%
 # cat = []
@@ -165,132 +175,243 @@ IMAGES_DATASET = PROJECT_PATH + "dataset/images/"
 # cat_by_folder = sorted(cat, key=lambda k: k['cat'])
 # cats = sorted(list({i['cat'] for i in cat_by_folder}))
 
-
-# #%%
-# import os
-
-# def rename_folders_Camel(_folder_path):
-#     for item in os.listdir(_folder_path):
-#         item_path = os.path.join(_folder_path, item)
-#         if os.path.isdir(item_path):
-#             new_name = item[0].upper() + item[1:]
-#             new_path = os.path.join(_folder_path, new_name)
-#             os.rename(item_path, new_path)
-
-# #%%
-# for f in os.listdir(FRUIT_360_DATASET + 'test'):
-#     if os.path.isdir(FRUIT_360_DATASET + 'test/'+f):
-#         for i in os.listdir(FRUIT_360_DATASET + 'test/'+f):
-#             file_name = i.split('.')[0]
-#             ext = i.split('.')[1]
-#             os.rename(FRUIT_360_DATASET + 'test/'+f+'/'+i,
-#                         FRUIT_360_DATASET + 'test/'+f+'/'+'360_'+ file_name +'.'+ext)
-# %%
-# # first word fruit having no multiple fruit in image
-# to_remove = [
-# 'Cactus',
-#  'Cantaloupe',
-#  'Carambula',
-#  'Galia',
-#  'Chestnut',
-#  'Granadilla',
-#  'Grapefruit',
-#  'Guava',
-#  'Huckleberry',
-#  'Kaki',
-#  'Kohlrabi',
-#  'Kumquats',
-#  'Limes',
-#  'Lychee',
-#  'Mangostan',
-#  'Maracuja',
-#  'Pepino',
-#  'Pepper',
-#  'Pepper',
-#  'Pepper',
-#  'Pepper',
-#  'Physalis',
-#  'Plum',
-#  'Potato Red',
-#  'Quince',
-#  'Rambutan',
-#  'Redcurrant',
-#  'Salak',
-#  'Tamarillo',
-#  'Tangelo',
-#  'Walnut',
-#  'Nut',
-#  'Cocos',
-#  'Peas',
-
-# ]
-# for d in os.listdir(PROJECT_PATH + "dataset/train"):
-#     if os.path.isdir(PROJECT_PATH + "dataset/train/"+d):
-#         if d.split()[0] in to_remove:
-#             shutil.rmtree(PROJECT_PATH + "dataset/train/"+d)
-
-
 #%%
-has_no_multiple= [
- 'Blueberry',
- 'Clementine',
- 'Dates',
- 'Fig',
- 'Hazelnut',
- 'Mandarine',
- 'Peach',
- 'Raspberry',]
-
-
-has_no_multiple_2= [
- 'Apricot',
- 'Avocado',]
-
-
 # Create an ImageDataGenerator with augmentation settings
 import numpy as np
-from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+
 datagen = ImageDataGenerator(
-    rotation_range=40,       # Randomly rotate images by up to 30 degrees
-    zoom_range=[1.0, 1.2],   # Zoom-out only by up to 20% as the training image have max zoom in
-    width_shift_range=0.02,  # Randomly shift images horizontally by up to 2%
-                                #we don't want to alter the shape too much 
-    height_shift_range=0.02, # Randomly shift images vertically by up to 2%
-    fill_mode='nearest',     # Fill mode for handling empty pixels
-    brightness_range=[0.8, 1.2],  # Randomly adjust brightness within a range
-    rescale=1.0/255.0,        # Rescale pixel values to [0, 1] (if not already)
-    horizontal_flip=True,    # Flip images horizontally
-)
+    width_shift_range=0.02,
+    height_shift_range=0.02,  # Shift the images vertically by up to 10% of their height
+    shear_range=0.01,         # Shear the images by up to 10 degrees
+    zoom_range=0.1,
+    fill_mode='nearest',
+    brightness_range=[0.8, 1.2],
+    rescale=1.0/255.0,
+    horizontal_flip=True,
+    vertical_flip=True,
+    )
+import math
+def augment_images(directory, max_files, datagen):
+    current_file_count = len([f for f in os.listdir(directory) if f.lower().endswith(('.png', '.jpg', '.jpeg', '.webp'))])
+    nb_to_create = max_files - current_file_count
+    augmentations_per_image = math.ceil(nb_to_create / current_file_count)
+    generated_images = 0
+    for image_file in os.listdir(directory):
+        if generated_images >= nb_to_create:
+            break
+        if image_file.lower().endswith(('.png', '.jpg', '.jpeg', '.webp')):
+            image_path = os.path.join(directory, image_file)
+            image = load_img(image_path)
+            image = img_to_array(image)
+            image = np.expand_dims(image, axis=0)
 
-for s in ['train', 'test']:
-    for d in os.listdir(PROJECT_PATH + "dataset/"+s):
-        if os.path.isdir(PROJECT_PATH + "dataset/" +s +"/"+d):
-            if d.split()[0] not in has_no_multiple +has_no_multiple_2:
-                list_file = [PROJECT_PATH + "dataset/"+s+ "/"+d+"/"+f for f in os.listdir(PROJECT_PATH + "dataset/"+s+"/"+d)]
-                nb_file = len(list_file)
-                nb_to_generate = int((800-534)/nb_file) if s =='train' else int((200-134)/nb_file)
-                for image_file in list_file:
-                    img = load_img(image_file)
-                    x = img_to_array(img)
-                    x = np.expand_dims(x, axis=0)
-                    # Generate augmented images and save them
-                    i = 0
-                    for batch in datagen.flow(x, batch_size=1):
-                        augmented_image = array_to_img(batch[0])
-                        file_ext = image_file.split(".")[-1]
-                        augmented_image.save(os.path.join(PROJECT_PATH + "dataset/" +s+"/"+d, f'{image_file.split("/")[-1].split(".")[0]}_augmented_{i}.' + file_ext))
-                        i += 1
-                        if i >= nb_to_generate: 
-                            break
+            # Augment the image
+            p = 0
+            for batch in datagen.flow(image, batch_size=1, save_to_dir=directory, save_prefix='aug_', save_format='jpg'):
+                generated_images += 1
+                p+=1
+                if generated_images >= max_files or p>augmentations_per_image:
+                    break
+    print(f"Generated {generated_images} images for {directory} \n")         
+# for d in os.listdir(FINAL_DATASET_224+'test'):
+#     augment_images(FINAL_DATASET_224+'test/'+d, 200, datagen)
 
+
+import multiprocessing
+
+def process_directory(directory):
+    # Your method logic here
+    print(f"Processing {directory}")
+
+def start_process_for_directory(directory, max_files):
+    process = multiprocessing.Process(target=augment_images, args=(directory,max_files,datagen))
+    process.start()
+    return process
+
+directories = os.listdir(FINAL_DATASET_224+'train/')  # List of directories
+
+# Start a process for each directory
+processes = [start_process_for_directory(FINAL_DATASET_224+'train/'+dir, 800) for dir in directories]
+
+# Wait for all processes to finish
+for process in processes:
+    process.join()
+
+print("All directories processed for test.")
 
 # %%
 
 for d in sorted(os.listdir(PROJECT_PATH + "dataset/test/")):
-    if os.path.isdir(PROJECT_PATH + "dataset/test/"+d) and d.split()[0] in has_no_multiple:
+    if os.path.isdir(PROJECT_PATH + "dataset/test/"+d) and d.split()[0] :
         list_file = sorted([PROJECT_PATH + "dataset/test/"+d+"/"+f for f in os.listdir(PROJECT_PATH + "dataset/test/"+d)])
         to_kept = int(len(list_file)/200)
         for n in range(len(list_file)):
             if n%to_kept!=0:
                 os.remove(list_file[n])
+# %%
+
+import os
+import random
+
+def reduce_files_to_limit(directory, file_limit=800):
+    """
+    Randomly delete files in the specified directory until the number of files is equal to file_limit.
+    Args:
+    - directory (str): The path to the directory.
+    - file_limit (int): The maximum number of files to retain in the directory.
+    """
+    # Get a list of files in the directory
+    files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+    
+    # Check if the number of files exceeds the limit
+    while len(files) > file_limit:
+        # Randomly select a file to delete
+        file_to_delete = random.choice(files)
+        
+        # Delete the file
+        os.remove(os.path.join(directory, file_to_delete))
+        
+        # Update the files list
+        files.remove(file_to_delete)
+
+# Example usage
+directory_path = '/path/to/your/directory'
+for d in os.listdir(PROJECT_PATH + "dataset/train/"):
+    if os.path.isdir(PROJECT_PATH + "dataset/train/"+d):
+        reduce_files_to_limit(PROJECT_PATH + "dataset/train/"+d, 800)
+
+
+for d in os.listdir(PROJECT_PATH + "dataset/test/"):
+    if os.path.isdir(PROJECT_PATH + "dataset/test/"+d):
+        reduce_files_to_limit(PROJECT_PATH + "dataset/test/"+d, 200)
+
+# %%
+import os
+from PIL import Image
+
+def find_min_image_shape(root_dir):
+    min_shape = None
+    dc ={}
+    for subdir, dirs, files in os.walk(root_dir):
+        for file in files:
+            try:
+                filepath = os.path.join(subdir, file)
+                with Image.open(filepath) as img:
+                    dc[img.size] = dc.get(img.size, 0) + 1
+                    if min_shape is None:
+                        min_shape = img.size  # img.size is in (width, height) format
+                    else:
+                        # Compare current image's size to the minimum found so far
+                        min_shape = tuple(min(s, m) for s, m in zip(img.size, min_shape))
+            except IOError:
+                # If the file cannot be opened as an image, skip it
+                print(f"Skipping non-image file: {filepath}")
+            except Exception as e:
+                print(f"Error processing file {filepath}: {e}")
+
+    # Return in (height, width) format
+    return [min_shape[1], min_shape[0] if min_shape else [None,None]], dc
+
+# Example usage:
+root_dir = FINAL_DATASET
+min_shape, dc = find_min_image_shape(root_dir)
+print(f"The minimum image shape in the directory and subdirectories is: {min_shape}")
+for k,v in {key: dc[key] for key in sorted(dc)}.items():
+    print(k,v)
+
+# %%
+import os
+
+def find_min_files_directory(root_dir):
+    min_files = float('inf')
+    min_dir = None
+    res = []
+    for subdir, dirs, files in os.walk(root_dir):
+        if dirs:
+            continue
+        file_count = sum([os.path.isfile(os.path.join(subdir, f)) for f in files])
+        res.append((subdir, file_count))
+
+    return  res
+
+# Example usage
+root_dir = FINAL_DATASET_224+'/train'
+ll = find_min_files_directory(root_dir)
+for tu in ll:   
+    directory_with_min_files,min_files_count = tu  
+    print(f"Directory with the minimum number of files: {directory_with_min_files.split('/')[-1]}")
+    print(f"Number of files in this directory: {min_files_count} \n")
+
+# %%
+import os
+from PIL import Image
+
+def remove_min_image_shape(root_dir):
+    min_shape = None
+    dc ={}
+    for subdir, dirs, files in os.walk(root_dir):
+        for file in files:
+            try:
+                filepath = os.path.join(subdir, file)
+                if filepath.endswith(".png") or filepath.endswith(".jpg"):
+                    # Open the image and check its shape
+                    with Image.open(filepath) as img:
+                        size = img.size
+                        height, width = size[0], size[1]
+                    if height<224 or width<224:
+                        os.remove(filepath)
+                elif height>width*2 or width>height*2:
+                    os.remove(filepath)
+                
+                        
+               
+            except IOError:
+                # If the file cannot be opened as an image, skip it
+                print(f"Skipping non-image file: {filepath}")
+            except Exception as e:
+                print(f"Error processing file {filepath}: {e}")
+
+remove_min_image_shape('/Users/tech/Downloads/archive/Fruit-262')
+
+
+#%%
+from PIL import Image
+
+def resize_and_pad(path, dir, cat,f, target_size=(224, 224), fill_color=(0, 0, 0)):
+
+    img_path = path+dir+'/'+cat+'/'+f
+    file_name = f.split('/')[-1]
+    name = file_name.split('.')[0]
+    ext = file_name.split('.')[1]
+    img= Image.open(img_path)
+    if img.mode == 'P':
+        img = img.convert('RGBA')
+
+    # Calculate the ratio to resize the image
+    ratio = min(target_size[0] / img.size[0], target_size[1] / img.size[1])
+    new_size = (int(img.size[0] * ratio), int(img.size[1] * ratio))
+
+    # Resize the image
+    img = img.resize(new_size, Image.Resampling.LANCZOS)
+
+    # Convert RGBA images to RGB to avoid issues with JPEG format
+    if img.mode == 'RGBA':
+        background = Image.new('RGB', img.size, fill_color)
+        background.paste(img, mask=img.split()[3])  # 3 is the alpha channel
+        img = background
+
+    # Create a new image and paste the resized image onto the center
+    new_img = Image.new('RGB', target_size, fill_color)
+    new_img.paste(img, ((target_size[0] - new_size[0]) // 2, (target_size[1] - new_size[1]) // 2))
+    new_img.save(FINAL_DATASET_224+dir+'/'+cat+'/'+name+'_224.'+ext)
+
+for ds in ['train','test']:
+    print('\n ##############')
+    print(ds)
+    for d in os.listdir(FINAL_DATASET+ds):
+        print(d)
+        for f in os.listdir(FINAL_DATASET+ds+'/'+d):
+            resize_and_pad(FINAL_DATASET,ds,d,f)
+
 # %%
